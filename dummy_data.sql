2a335a267f51e916d335567f1a70935af22c3028
Swati Shree <swatishree26@gmail.com>
2020-10-11 20:21:00 +0530

    Growth Engg Assignment-Sql dummy data

---
+++ dummy_data.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE fooddetails;
+
+INSERT INTO  food_categories VALUES (1, 'Chicken and Fish Burger');
+INSERT INTO  food_categories VALUES (2, 'King Delight Burger');
+INSERT INTO  food_categories VALUES (3, 'Veggie Burger');
+INSERT INTO  food_categories VALUES (4, 'Sweets');
+INSERT INTO  food_categories VALUES (5, 'Beverages');
+
+
+INSERT INTO  food_items VALUES (1, 1, 'Chicken Tikka Burger', 'Burger featured with Chicken Tikka patty topped with juicy tomato', 'NonVeg', 200);
+INSERT INTO  food_items VALUES (2, 1, 'Fish Tikka Burger', 'Burger featured with Fish Tikka patty topped with juicy tomato', 'NonVeg', 400);
+INSERT INTO  food_items VALUES (3, 3, 'Paneer Tikka Burger', 'Burger featured with Paneer Tikka patty topped with juicy tomato, fresh lettuce', 'Veg', 100);
+INSERT INTO  food_items VALUES (4, 3, 'Aloo Tikki Burger', 'The use of veggies like tomato, onion, and lettuce adds subtle taste', 'Veg', 80);
+INSERT INTO  food_items VALUES (5, 5, 'Aam Panna', 'A raw mango appetizer with fresh mint and coriander', 'Veg', 80);
+INSERT INTO  food_items VALUES (6, 5, 'Shikanji', 'A refreshing cumin appetizer served chilled with lemon juice & mint', 'Veg', 70);
+INSERT INTO  food_items VALUES (7, 2, 'Mixed Zinger Burger Duo', 'Enjoy the signature Classic Chicken Zinger & new Spicy Chicken Zinger in this Duo', 'NonVeg', 295);
+INSERT INTO  food_items VALUES (8, 2, 'Maharaja Burger', 'Xtraa Chicken with smoky flavour and french buns', 'NonVeg', 295);
+INSERT INTO  food_items VALUES (9, 4, 'Motichur Laddoo', 'Made with Pure Ghee', 'Veg', 200);
+INSERT INTO  food_items VALUES (10, 4, 'Kaju Katli', 'Made with Pure Ghee and Cashew', 'Veg', 450);
+
+
